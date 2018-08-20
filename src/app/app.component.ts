7834fed17d7e4ba30ee5242370ee5dac3cd64dd2
eyas2014 <yamingshao2014@gmail.com>
2018-08-20 16:46:55 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//import * as d3 from 'd3';
+import {DataService} from './data.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deepseqView';
+
+  DividedMergedData;
+
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(){
+  	var data=this.dataService.getData();
+  	var peakWidth=Math.ceil(data.length/1500);
+  	var numOfPeaks=Math.ceil(data.length/peakWidth);
+  	var numOfPanels=Math.ceil(numOfPeaks/250);
+
+  	var MergedData=[];
+  	for(var i=0; i<numOfPeaks; i++){
+  		MergedData.push(Math.max(...data.slice(i*peakWidth, (i+1)*peakWidth)));
+  	}
+
+  	this.DividedMergedData=[];
+  	for(var i=0; i<numOfPanels-1; i++){
+  		this.DividedMergedData[i]=MergedData.slice(250*i, 250*(i+1));
+  	}
+  	this.DividedMergedData[numOfPanels-1]=MergedData.slice(250*(numOfPanels-1), numOfPeaks);
+
+  	console.log(this.DividedMergedData);
+
+  }
+
+
 }
